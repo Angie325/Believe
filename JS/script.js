dc8e496157dd233d65266053fee44e4f32cc287f
Angie <angelaapondi23@gmail.com>
2020-07-19 16:44:06 +0300

    add initial page structure and title

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+let number1= parseInt(prompt("Enter Gender"))
+let number2= parseInt(prompt("Enter month"))
+let number3= parseInt(prompt("Enter first two digits of century of birth"))
+let number4= parseInt(prompt("Enter last two digits of  Year of birth"))
+let number5= parseInt(prompt("Enter day of month"))
+let number6= parseInt(prompt ("Enter modulus function"))    
+
+
+
+
+    
+
+    
